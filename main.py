b52e05f1bb8ed74819449cdbdf6f435e19ee5788
cianuro123 <66453327+cianuro123@users.noreply.github.com>
2025-06-17 12:03:59 -0300

    Update main.py

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ app.mount("/static", StaticFiles(directory="Frontend/static"), name="static")
 # CORS setup
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://gestor-de-tareas-r39h.onrender.com"],  # ⚠️ en producción, mejor usar ["https://miweb.com"]
+    allow_origins=["https://gestor-de-tareas-r39h.onrender.com", "*"],  # ⚠️ en producción, mejor usar ["https://miweb.com"]
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
