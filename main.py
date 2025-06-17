b8be950ce2b66ad9c29c28fc2236bf6e43d2a0fa
cianuro123 <66453327+cianuro123@users.noreply.github.com>
2025-06-17 03:21:09 -0300

    f->F

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from Backend.routers import tareas, users
 
 
 app = FastAPI()
-app.mount("/", StaticFiles(directory="frontend", html=True), name="static")
+app.mount("/", StaticFiles(directory="Frontend", html=True), name="static")
 # CORS setup
 app.add_middleware(
     CORSMiddleware,
