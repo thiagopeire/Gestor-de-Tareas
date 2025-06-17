3547d665ecbe9aaad03715d38b241cc18ff42cb2
cianuro123 <66453327+cianuro123@users.noreply.github.com>
2025-06-17 03:07:38 -0300

    move main

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.staticfiles import StaticFiles
-from routers import tareas, users
+from Backend.routers import tareas, users
 
 
 app = FastAPI()
