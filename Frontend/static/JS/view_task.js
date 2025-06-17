9c27269ebce100948f738d1d81530d78b720950e
cianuro123 <66453327+cianuro123@users.noreply.github.com>
2025-06-17 16:19:25 -0300

    Update view_task.js

--- Frontend/static/JS/view_task.js
+++ Frontend/static/JS/view_task.js
@@ -31,7 +31,7 @@ async function fetchTask() {
 
     } catch(error) {
 
-          console.log(error.details) // Redirigir a la página principal si hay un error
+          console.log(error) // Redirigir a la página principal si hay un error
         }
 }
 const errorMsg = document.getElementById("error-msg");
